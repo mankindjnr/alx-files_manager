6770af3207925ad3167e993f83eaefd58471a3a5
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-31 04:40:29 -0700

    mongodb utils

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const usersCollection = this.client.db().collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    const filesCollection = this.client.db().collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+dbClient.connect();
+
+module.exports = dbClient;
+
